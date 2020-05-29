af26ca5c05efc579cb60015531899046d1b47475
Naveen <navi.sagar.88@gmail.com>
2020-05-29 19:07:35 +0530

    commit 5 section complete

---
+++ CoreJavaQA2QE/src/BasicNuancesofJavaJVMJDKLoopsConditionalStatements/megamartdiscount.java
@@ -0,0 +1,10 @@
+package BasicNuancesofJavaJVMJDKLoopsConditionalStatements;
+
+public class megamartdiscount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
